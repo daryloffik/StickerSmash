be0bf425ce5ecdf35af30c472f1c6e194db543f2
daryloffik <quenumdaryl28@gmail.com>
2025-01-02 22:11:29 +0100

    feat:add button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
 import { Text, View, StyleSheet } from 'react-native';
  import { Link } from 'expo-router'; 
+ import { Image } from 'expo-image';
+ import ImageViewer from "@/components/ImageViewer";
+ import Button from '@/components/Button';
+
+ const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+  <View style={styles.imageContainer}>
+  <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -26,5 +34,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+   image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
